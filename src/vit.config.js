dc45f65b2f8436b5f83bc86891b3d8e22fdb08ff
IMDR <idelosreyes158@gmail.com>
2024-10-10 18:48:00 +0800

    latest commit

--- src/vit.config.js
+++ src/vit.config.js
@@ -1,10 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/IMDR/', // Update to match your GitHub repository name
-  build: {
-    outDir: 'dist' // Ensure this matches your deploy script's output directory
-  },
-  // Optional: Add any additional plugins or settings here
+  server: {
+    host: true, // Allows connections from outside localhost
+    port: 4173 // This should match the port Render uses
+  }
 });
